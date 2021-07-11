4fa0eb552d90a01859d51407918ad08af7277e89
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2021-07-11 19:33:41 +0700

    bump version → 1.3.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="revolt",
-    version="1.2.0",
+    version="1.3.0",
     description="Value substitutor for district42 schema",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
