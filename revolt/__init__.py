0a66988ceddfe89409c78951b202cba194516b7d
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2021-09-24 22:12:10 +0700

    reimport SubstitutorValidator

--- revolt/__init__.py
+++ revolt/__init__.py
@@ -4,10 +4,11 @@ from district42 import GenericSchema
 from district42.types import Schema
 
 from ._substitutor import Substitutor
+from ._validator import SubstitutorValidator
 from ._version import version
 
 __version__ = version
-__all__ = ("substitute", "Substitutor",)
+__all__ = ("substitute", "Substitutor", "SubstitutorValidator",)
 
 _substitutor = Substitutor()
 
