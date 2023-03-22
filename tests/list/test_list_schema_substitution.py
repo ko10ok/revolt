56de778280b4c5b579ac21612d6a7cb477e5ca97
Юрий Сагитов <yu.sagitov@2gis.ru>
2023-03-22 21:46:08 +0300

    #6 list schema substitution tests

---
+++ tests/list/test_list_schema_substitution.py
@@ -0,0 +1,168 @@
+from copy import deepcopy
+
+import pytest
+from baby_steps import then, when, given
+from district42 import schema, GenericSchema
+from pytest import raises
+
+from revolt import substitute
+from revolt.errors import SubstitutionError
+
+
+def test_schema_list_with_schema_list_substitution():
+    with given:
+        sch = schema.list
+
+    with when:
+        res = substitute(sch, sch)
+
+    with then:
+        assert res == sch
+        assert id(res) != id(sch)
+
+
+def test_schema_list_with_schema_list_with_value_substitution():
+    with given:
+        sch = schema.list
+
+    with given:
+        clarification_type = schema.list([])
+
+    with when:
+        res = substitute(sch, clarification_type)
+
+    with then:
+        assert sch == schema.list
+        assert res == clarification_type
+        assert id(res) != id(clarification_type)
+        assert id(res) != id(sch)
+
+
+def test_schema_list_with_value_with_schema_list_with_same_value_substitution():
+    with given:
+        value = []
+
+    with given:
+        sch = schema.list(value)
+
+    with given:
+        clarification_type = schema.list(value)
+
+    with when:
+        res = substitute(sch, clarification_type)
+
+    with then:
+        assert res == clarification_type
+        assert id(res) != id(clarification_type)
+        assert id(res) != id(sch)
+
+
+def test_schema_list_with_value_with_schema_list_with_different_incomparable_value_substitution_error():
+    with given:
+        value = [1, 2, 3]
+        another_value = [3, 2, 1]
+
+    with given:
+        sch = schema.list(value)
+
+    with given:
+        clarification_type = schema.list(another_value)
+
+    with when, raises(Exception) as exception:
+        substitute(sch, clarification_type)
+
+    with then:
+        assert exception.type is SubstitutionError
+        assert sch == schema.list(value)
+
+
+def test_schema_list_with_value_with_schema_list_with_different_incomparable_value_over_subtype_substitution_error():
+    with given:
+        value = [schema.int]
+        another_value = [schema.float]
+
+    with given:
+        sch = schema.list(value)
+
+    with given:
+        clarification_type = schema.list(another_value)
+
+    with when, raises(Exception) as exception:
+        substitute(sch, clarification_type)
+
+    with then:
+        assert exception.type is SubstitutionError
+        assert sch == schema.list(value)
+
+
+def test_schema_list_with_value_with_schema_list_with_different_incomparable_value_on_subtype_value_substitution_error():
+    with given:
+        value = [schema.int(1)]
+        another_value = [schema.int(2)]
+
+    with given:
+        sch = schema.list(value)
+
+    with given:
+        clarification_type = schema.list(another_value)
+
+    with when, raises(Exception) as exception:
+        substitute(sch, clarification_type)
+
+    with then:
+        assert exception.type is SubstitutionError
+        assert sch == schema.list(value)
+
+
+@pytest.mark.parametrize("basic_type,clarification_type", [
+    (
+        schema.list([..., schema.int, ...]),
+        schema.list([..., schema.float, ...])
+    ),
+    (
+        schema.list([..., schema.float, schema.int, ...]),
+        schema.list([..., schema.int, schema.float, ...])
+    ),
+    (
+        schema.list([..., schema.float, schema.int, ...]),
+        schema.list([..., schema.int, ...]).len(1)
+    ),
+])
+def test_schema_list_with_value_with_schema_list_with_different_incomparable_value_on_partial_value_substitution_error(
+    basic_type: GenericSchema,
+    clarification_type: GenericSchema
+):
+    with given:
+        sch = deepcopy(basic_type)
+
+    with when, raises(Exception) as exception:
+        substitute(sch, clarification_type)
+
+    with then:
+        assert exception.type is SubstitutionError
+        assert sch == schema.list(value)
+
+
+@pytest.mark.parametrize("clarification_type", [
+    schema.any,
+    schema.bool,
+    schema.bytes,
+    schema.const,
+    schema.float,
+    schema.int,
+    schema.dict,
+    schema.none,
+    schema.str,
+])
+def test_schema_list_with_different_schema_substitution_error(
+    clarification_type: GenericSchema
+):
+    with given:
+        sch = schema.list
+
+    with when, raises(Exception) as exception:
+        substitute(sch, clarification_type)
+
+    with then:
+        assert exception.type is SubstitutionError
+        assert sch == schema.list
---
+++ tests/list/test_list_schema_with_comparable_list_substitution.py
@@ -0,0 +1,158 @@
+from copy import deepcopy
+
+import pytest
+from baby_steps import then, when, given
+from district42 import schema, GenericSchema
+from pytest import raises
+
+from revolt import substitute
+from revolt.errors import SubstitutionError
+
+"""
+ListSchema ranging via one steps clarifying mutations tree
+    
+    schema.list == schema.list([...]) == schema.list(schema.any)
+        
+    schema.list([...]) > schema.list.len(3) == schema.list([schema.any] * 3)
+    
+    schema.list([...]) > 
+        schema.list([..., schema.int, ...]) >
+        schema.list([..., schema.int, schema.float, ...]) > 
+        schema.list([..., schema.int, schema.float]) >
+        schema.list([schema.int, schema.float])
+    
+    schema.list([..., schema.int, schema.float, ...]) >
+        schema.list([schema.int, schema.float, ...]) >
+        schema.list([schema.int, schema.float])
+    
+    schema.list([..., schema.int, ...]) > schema.list([..., schema.int, ...]).len(3) 
+    
+    schema.list > schema.list([])
+    
+    schema.list(schema.any) > schema.list([schema.any]) == schema.list(schema.any).len(1)
+    
+    schema.list(schema.any) > schema.list(schema.int)
+"""
+
+
+@pytest.mark.parametrize("basic_type,clarification_type", [
+    (schema.list, schema.list([...])),
+    (schema.list, schema.list(schema.any)),
+    (schema.list([...]), schema.list.len(3)),
+    (schema.list.len(3), schema.list([schema.any] * 3)),
+    (schema.list([schema.any] * 3), schema.list.len(3)),
+    (schema.list([...]), schema.list([..., schema.int, ...])),
+    (
+        schema.list([..., schema.int, ...]),
+        schema.list([..., schema.int, schema.float, ...])
+    ),
+    (
+        schema.list([..., schema.int, schema.float, ...]),
+        schema.list([..., schema.int, schema.float])
+    ),
+    (
+        schema.list([..., schema.int, schema.float]),
+        schema.list([schema.int, schema.float])
+    ),
+    (
+        schema.list([..., schema.int, schema.float, ...]),
+        schema.list([schema.int, schema.float, ...])
+    ),
+    (
+        schema.list([schema.int, schema.float, ...]),
+        schema.list([schema.int, schema.float])
+    ),
+    (
+        schema.list([..., schema.int, ...]),
+        schema.list([..., schema.int, ...]).len(3)
+    ),
+    (
+        schema.list([...]),
+        schema.list([])
+    ),
+    (
+        schema.list([...]),
+        schema.list([])
+    ),
+    (
+        schema.list([..., schema.str, schema.int, schema.str, ...]),
+        schema.list([..., schema.str, schema.int, schema.str, ...]).len(3)
+    ),
+    (schema.list(schema.any), schema.list([schema.any])),  # ????
+    (schema.list([schema.any]), schema.list(schema.any)),  # ????
+    (schema.list(schema.any), schema.list(schema.int)),
+    (schema.list(schema.any), schema.list(schema.int(1))),
+])
+def test_dict_schema_clarification_with_dict_schema_with_subset_values_substitution(
+    basic_type: GenericSchema,
+    clarification_type: GenericSchema
+):
+    with given:
+        sch = deepcopy(basic_type)
+
+    with when:
+        res = substitute(sch, clarification_type)
+
+    with then:
+        assert res == clarification_type
+        assert sch == basic_type
+        assert id(res) != id(clarification_type)
+        assert id(res) != id(sch)
+
+
+@pytest.mark.parametrize("basic_type,clarification_type", [
+    # (schema.list([...]), schema.list), ???
+    # (schema.list(schema.any), schema.list, ), ???
+    (schema.list.len(3), schema.list([...])),
+    (schema.list([schema.any] * 3), schema.list.len(3)),
+    (schema.list.len(3), schema.list([schema.any] * 3)),
+    (schema.list([..., schema.int, ...]), schema.list([...])),
+    (
+        schema.list([..., schema.int, schema.float, ...]),
+        schema.list([..., schema.int, ...])
+    ),
+    (
+        schema.list([..., schema.int, schema.float]),
+        schema.list([..., schema.int, schema.float, ...])
+    ),
+    (
+        schema.list([schema.int, schema.float]),
+        schema.list([..., schema.int, schema.float])
+    ),
+    (
+        schema.list([schema.int, schema.float, ...]),
+        schema.list([..., schema.int, schema.float, ...])
+    ),
+    (
+        schema.list([schema.int, schema.float]),
+        schema.list([schema.int, schema.float, ...])
+    ),
+    (
+        schema.list([..., schema.int, ...]).len(3),
+        schema.list([..., schema.int, ...])
+    ),
+    (
+        schema.list([..., schema.str, schema.int, schema.str, ...]).len(3),
+        schema.list([..., schema.str, schema.int, schema.str, ...]),
+    ),
+    (schema.list(schema.int), schema.list(schema.any),),
+    (schema.list(schema.int(1)), schema.list(schema.any)),
+
+    (
+        schema.list([..., schema.float, schema.int, ...]),
+        schema.list([..., schema.int]).len(2)
+    ),
+])
+def test_schema_dict_clarification_with_dict_schema_with_superset_values_substitution_error(
+    basic_type: GenericSchema,
+    clarification_type: GenericSchema
+):
+    with given:
+        sch = deepcopy(basic_type)
+
+    with when, raises(Exception) as exception:
+        substitute(sch, clarification_type)
+
+    with then:
+        assert exception.type is SubstitutionError
+        assert sch == basic_type
